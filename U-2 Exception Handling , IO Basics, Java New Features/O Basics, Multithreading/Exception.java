62724942ac4ed2bee869e3b920fb0c264e3483b3
Abdul Rahman Ali <gyansuccesstreasury@gmail.com>
2026-04-10 14:44:01 +0530

    Exception Code / 0 Exception

--- U-2 Exception Handling , IO Basics, Java New Features/O Basics, Multithreading/Exception.java
+++ U-2 Exception Handling , IO Basics, Java New Features/O Basics, Multithreading/Exception.java
@@ -1,14 +1,13 @@
-Exceptionclass Exception
+class Exception
 {
 	public static void main ( String args [ ] ) 
 	{
 		int d = 20;
 		d = d/0;
 		System.out.println("d = "+d);
-	/* 
-	Exception in thread "main" java.lang.ArithmeticException: / by zero
-      	  at Exception.main(Exception.java:6)
-	*/
-
+/* 
+Exception in thread "main" java.lang.ArithmeticException: / by zero
+        at Exception.main(Exception.java:6)
+*/
 	}
-} 
+}
