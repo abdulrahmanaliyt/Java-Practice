d4c53f185a0725c49c00b5ba148f869aeedfff8e
Abdul Rahman Ali <gyansuccesstreasury@gmail.com>
2026-04-11 20:54:44 +0530

    byte-stream file reader
    Add FileReading_Bytes_Stream class and Sample.txt for testing byte-level operations
    
    File	                                                Purpose
    
    FileReading_Bytes_Stream.java	Demonstrates read(), skip(), and available() methods.
    Sample.txt                                       The source data file used by the Java program.

---
+++ U-2 Exception Handling , IO Basics,Multithreading/FileReading_Bytes_Stream.java
@@ -0,0 +1,45 @@
+//First of all create file with name Sample.txt in CurrentWorkinfDirectory
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileReading_Bytes_Stream {
+	public static void main(String [ ] args) throws FileNotFoundException, IOException {
+		System.out.println("Looking in: " + new java.io.File(".").getAbsolutePath());
+		
+		FileInputStream fis = new FileInputStream("Sample.txt");
+		
+		System.out.println("Available Bytes in file : "+fis.available());
+
+		int byteRead = fis.read( );
+		System.out.println((char)byteRead);
+		byteRead = fis.read( );
+		System.out.println((char)byteRead);
+		byteRead = fis.read( );
+		System.out.println((char)byteRead);
+		byteRead = fis.read( );
+		System.out.println((char)byteRead);
+
+		byte [ ] fourBytesRead = new byte [ 4 ];
+		fis.read(fourBytesRead);
+		System.out.println(new String(fourBytesRead));
+
+		System.out.println("Available Bytes: "+fis.available( ) );
+
+		fis.skip(2);
+		fis.read(fourBytesRead);
+		System.out.println(new String(fourBytesRead));
+
+		System.out.println(fis.read(fourBytesRead,2,2));
+		fis.read(fourBytesRead, 0, 2);
+		System.out.println(new String(fourBytesRead));
+
+		System.out.println("Available Bytes: "+fis.available( ) );
+
+		byte [ ] twoBytesRead = fis.readNBytes(2);
+		System.out.println(new String(twoBytesRead));
+
+		fis.close( );
+	}
+}
