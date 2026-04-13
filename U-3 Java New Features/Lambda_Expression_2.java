17679c7614f4f834169ca23080301dcf37c58df5
Abdul Rahman Ali <gyansuccesstreasury@gmail.com>
2026-04-13 17:58:25 +0530

    use concise lambda syntax and @FunctionalInterface
    refactor: streamline functional interface with concise lambda syntax
    
    Added @FunctionalInterface annotation to IOperations for type safety.
    
    Refactored lambda declaration to use inferred types and implicit returns.
    
    Demonstrated reassigning lambda logic to the same interface reference.
    
    A "Hidden" Benefit: Scope
    One thing to remember as you use these: Lambdas can "capture" variables from the code around them (effectively final variables), but they cannot modify them.
    
    Example:
    
    Java
    int multiplier = 3;
    IOperations ref = a -> a * multiplier; // This works!
    // multiplier = 5; // This would break the lambda above!

---
+++ U-3 Java New Features/Lambda_Expression_2.java
@@ -0,0 +1,29 @@
+//Functional Interfaces
+
+@FunctionalInterface
+interface IOperations
+{
+	int myOperation (int a );
+	// void anotherMethod(); // This would now cause a compiler error!
+}
+class Lambda_Expression_2
+{
+	public static void main (String [ ] args )
+	{
+		//Lambda_Expression declaration
+		//IOperations ref = (int a ) -> { return a*a; };	
+
+		// No 'int' needed, no braces, no 'return'
+		
+		/* more consize
+		if a lambda has only one expression, 
+		you can remove the int type (it's inferred), 
+		the curly braces, and the return keyword. */
+
+		IOperations ref = a -> a * a;
+		System.out.println(ref.myOperation(4));
+
+		ref = a -> a * 2;
+		System.out.println(ref.myOperation(4));
+	}
+}
