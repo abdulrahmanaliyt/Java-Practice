972566dacf28c2347ea913d0680f6ffc0ae113b6
Abdul Rahman Ali <gyansuccesstreasury@gmail.com>
2026-04-13 10:35:51 +0530

    Creating New Thread
    feat: create multiple threads using Thread(Runnable, String) constructor and handle InterruptedException
    
    feat: create multiple threads using Thread(Runnable, String) constructor and handle InterruptedException

---
+++ U-2 Exception Handling , IO Basics,Multithreading/Thread_Creation_Using_Runnable.java
@@ -0,0 +1,39 @@
+//Creating Thread Using 'Runnable' interface
+
+class MyThread implements Runnable 
+{
+	public void run () 
+	{
+		//System.out.println("Thread Starts .. ");
+		String threadName = Thread.currentThread( ).getName ( );
+		System.out.println("Current Thread Name : "+threadName+" statarts -- ");
+		int i;
+		for (i =0; i<5;i++) 
+		{
+			try {
+				Thread.sleep(500);
+				System.out.println("Thread IS Sleeping"+threadName+"s i = "+i);
+			}
+			catch (InterruptedException ie) 
+			{
+			ie.printStackTrace ( );
+			}
+		}
+		//System.out.println("i = "+i);
+		System.out.println("-- "+threadName+" ends.");
+	}
+}
+class Thread_Creation_Using_Runnable
+{
+	public static void main (String [ ] args )
+	{
+		MyThread obj1 = new MyThread ( );
+		MyThread obj2 = new MyThread ( );
+
+		Thread thread1 = new Thread ( obj1,"Thread 1");
+		Thread thread2 = new Thread ( obj2,"Thread 2");
+
+		thread1.start( );
+		thread2.start( );
+	}
+}			
