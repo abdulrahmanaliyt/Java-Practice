a7628549070dffb065bbd054536b2d2fd4fdfc93
Abdul Rahman Ali <gyansuccesstreasury@gmail.com>
2026-04-19 23:17:16 +0530

    Annotation Class Use

---
+++ U-3 Java New Features/Annotation_Class_Use.java
@@ -0,0 +1,30 @@
+@interface ClassAnnotation
+{
+	int noFields( );
+	int noMethods( );
+	int noConstructors( );
+}
+@ClassAnnotation(noFields=2,noMethods=2,noConstructors=1)
+class MyClass
+{
+	int attr1;
+	int attr2;
+
+	MyClass( int attr1, int  attr2)
+	{
+		this.attr1 = attr1;
+		this.attr2 = attr2;
+	}
+	void display ()
+	{
+		System.out.println(attr1+ ": "+attr2);
+	}
+}
+class Annotation_Class_Use
+{
+	public static void main (String args [ ] )
+	{
+		MyClass obj = new MyClass (23,24 );
+		obj.display( );
+	}
+}
